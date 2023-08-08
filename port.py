7a3bf8ea9de326a3127227571776c6331d5c7f28
jonahbebko <jonahbebko@outlook.com>
2023-08-08 09:37:22 -0400

    colorized ports

--- port.py
+++ port.py
@@ -1,3 +1,9 @@
+_COLORS = {
+    "red": "\033[1;31m",
+    "green": "\033[1;32m",
+    "reset": "\033[0;0m",
+}
+
 class PORT:
     
     def load(self) -> int:
@@ -12,7 +18,9 @@ class DECIMAL_OUTPUT:
         return 0
 
     def store(self, value: int) -> None:
-        print(f"{value} | 0x{value:02X} | 0b{value:08b}")
+        bin_value = f"{value:08b}"
+        bin_value = [_COLORS["green"] + bit + _COLORS["reset"] if bit == "1" else _COLORS["red"] + bit + _COLORS["reset"] for bit in bin_value]
+        print(f"{value:03d} | 0x{value:02X} | 0b{''.join(bin_value)}")
 
 class INPUT:
     
